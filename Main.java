f9ddf4fff876564c5baf39a87bff3f1e51924c0f
leonzoX <103598595+leonzoX@users.noreply.github.com>
2025-01-29 13:25:57 +0100

    Main

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import modelGestionale.*;
+import controllerGestionale.*;
+import viewGestionale.*;
+
+
+public class Main{
+	public static void main(String args[]){
+
+		ViewGestionale gui = new ViewGestionale("Gestionale",500,500);
+		
+
+
+
+
+		
+	}
+}
+
